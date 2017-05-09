b5a9dccfd980536cfb4c9a8e7e9c309f0dca2aae
BigBaws <thanthony@gmail.com>
2017-05-09 16:00:25 +0200

    Refactoring...

--- src/java/com/users/ApplicationConfig.java
+++ src/java/com/users/ApplicationConfig.java
@@ -24,18 +24,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(com.example.AnimalResource.class);
-        resources.add(com.example.ChatResource.class);
-        resources.add(com.example.HighscoreResource.class);
-        resources.add(com.example.LogicResource.class);
-        resources.add(com.example.LoginResource.class);
-        resources.add(com.example.MultiplayerLogicResource.class);
-        resources.add(com.example.MultiplayerResource.class);
-        resources.add(com.example.PaymentResource.class);
-        resources.add(com.example.ServiceResource.class);
-        resources.add(com.example.SingleplayerLogicResource.class);
-        resources.add(com.example.SingleplayerResource.class);
-        resources.add(com.example.UserResource.class);
         resources.add(com.users.ServiceResource.class);
         resources.add(hanganimals.rest.AnimalResource.class);
         resources.add(hanganimals.rest.ChatResource.class);
